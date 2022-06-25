8cd7a2ea8065ab5577bd28b4e016e4cafce7ef12
lesha1980 <lesha.ievenko@yandex.ua>
2022-06-25 19:15:57 +0300

    Init SingleDeck

--- Model/SingleDeck.cpp
+++ Model/SingleDeck.cpp
@@ -1,6 +1,13 @@
 #include "../libs.h"
+#include "Enums/StatusShip.h"
+#include "Enums/TypeShip.h"
 
 SingleDeck::SingleDeck(int x, int y)
 {
-	
+	this->_deck.setNumber(1);
+	this->_deck.setX(x);
+	this->_deck.setY(y);
+	this->_deck.setStatus(true);
+	this->_type_ship = TypeShip::SingleDeck;
+	this->_status_ship = StatusShip::Alive;
 }
