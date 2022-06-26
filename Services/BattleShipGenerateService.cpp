e4654e8fa168df2a49edaddc4ef1ae162e3e9b2a
lesha1980 <lesha.ievenko@yandex.ua>
2022-06-26 13:44:19 +0300

    Added two methods with function pointers BattleShipGenerateService

--- Services/BattleShipGenerateService.cpp
+++ Services/BattleShipGenerateService.cpp
@@ -2,7 +2,7 @@
 
 BattleShipGenerateService::BattleShipGenerateService()
 {
-	this->_ships = new Ship[10];
+	this->_ships = new Ship*[10];
 }
 
 Ship* BattleShipGenerateService::CreateSingleDeck(int x, int y)
@@ -24,3 +24,23 @@ Ship* BattleShipGenerateService::CreateFourDeck(int x, int y, bool oriental)
 {
 	return new FourDeck(oriental, x, y);
 }
+
+void BattleShipGenerateService::CreateDataSingleShip(Ship* (*SingleDeckCreatorPtr)(int, int), int x, int y, int index)
+{
+		Ship* _ship = SingleDeckCreatorPtr(x, y);
+		this->_ships[index] = _ship;
+}
+
+void BattleShipGenerateService::CreateDataDoubleShip(Ship* (*MultiDeckCreatorPtr)(int x, int y, bool oriental), int x, int y, bool oriental, int index)
+{
+	Ship* _ship = MultiDeckCreatorPtr(x, y, oriental);
+	this->_ships[index] = _ship;
+}
+
+Ship** BattleShipGenerateService::getShips()
+{
+	return this->_ships;
+}
+
+
+
--- Services/BattleShipGenerateService.h
+++ Services/BattleShipGenerateService.h
@@ -4,7 +4,7 @@
 
 class BattleShipGenerateService {
 
-	Ship* _ships;
+	Ship** _ships;
 
 public:
 
@@ -13,5 +13,8 @@ public:
 	Ship* CreateDoubleDeck(int x, int y, bool oriental);
 	Ship* CreateThreeDeck(int x, int y, bool oriental);
 	Ship* CreateFourDeck(int x, int y, bool oriental);
+	void CreateDataSingleShip(Ship* (*SingleDeckCreatorPtr)(int, int), int x, int y, int index);
+	void CreateDataDoubleShip(Ship* (*MultiDeckCreatorPtr)(int x, int y, bool oriental), int x, int y, bool oriental, int index);
+	Ship** getShips();
 
 };
